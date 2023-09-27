211a69d12338cf744e8f0d887325e7843b65d92f
Adrian Ferrandis <51866199+ferrandisme@users.noreply.github.com>
2023-09-27 20:51:05 +0200

    [FIX] @Repository

--- src/main/kotlin/me/ferrandis/textAnalyzer/repository/LanguageRepository.kt
+++ src/main/kotlin/me/ferrandis/textAnalyzer/repository/LanguageRepository.kt
@@ -2,6 +2,8 @@ package me.ferrandis.textAnalyzer.repository
 
 import me.ferrandis.textAnalyzer.model.LanguageData
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface LanguageRepository : ReactiveCrudRepository<LanguageData, Long> {
 }
--- src/main/kotlin/me/ferrandis/textAnalyzer/repository/SentimentRepository.kt
+++ src/main/kotlin/me/ferrandis/textAnalyzer/repository/SentimentRepository.kt
@@ -2,6 +2,8 @@ package me.ferrandis.textAnalyzer.repository
 
 import me.ferrandis.textAnalyzer.model.SentimentAnalysis
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface SentimentRepository : ReactiveCrudRepository<SentimentAnalysis, Long> {
 }
