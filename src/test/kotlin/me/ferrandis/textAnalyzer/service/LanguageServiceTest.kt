a2f67d956b2f7279d16c960a1a4e45b3e68c341d
Adrian Ferrandis <51866199+ferrandisme@users.noreply.github.com>
2023-09-27 22:09:18 +0200

    [TEST] getLanguage

--- src/test/kotlin/me/ferrandis/textAnalyzer/service/LanguageServiceTest.kt
+++ src/test/kotlin/me/ferrandis/textAnalyzer/service/LanguageServiceTest.kt
@@ -1,12 +1,22 @@
 package me.ferrandis.textAnalyzer.service
 
+import me.ferrandis.textAnalyzer.model.LanguageData
 import me.ferrandis.textAnalyzer.repository.LanguageRepository
 import opennlp.tools.langdetect.LanguageDetectorME
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.ArgumentMatchers
 import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.jupiter.MockitoExtension
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import reactor.core.publisher.Mono
 
+@SpringBootTest
+@ExtendWith(MockitoExtension::class)
 class LanguageServiceTest {
 
     lateinit var service: LanguageService;
@@ -14,7 +24,7 @@ class LanguageServiceTest {
     @Mock
     lateinit var repository: LanguageRepository;
 
-    @Mock
+    @Autowired
     lateinit var languageDetector: LanguageDetectorME
 
 
@@ -24,13 +34,17 @@ class LanguageServiceTest {
     }
 
     @Test
-    @Disabled
     fun getLanguage() {
-        /*assertEquals("spa", service.getLanguage("El sol brilla en el cielo azul").blockFirst()?.get(0)?.lang);
-        assertEquals("pob", service.getLanguage("O pôr do sol na praia é sempre uma vista deslumbrante").blockFirst()?.get(0)?.lang);
-        assertEquals("fra", service.getLanguage("La vie est belle").blockFirst()?.get(0)?.lang);
-        assertEquals("ita", service.getLanguage("La dolce vita").blockFirst()?.get(0)?.lang);
-        assertEquals("eng", service.getLanguage("Artificial Intelligence is very cool").blockFirst()?.get(0)?.lang);*/
+        Mockito.`when`(repository.save(ArgumentMatchers.any())).thenAnswer { invocation ->
+            val argument = invocation.getArgument<LanguageData>(0)
+            Mono.just(argument)
+        }
+
+        assertEquals("spa", service.getLanguage("El sol brilla en el cielo azul").block()?.languages?.get(0)?.lang);
+        assertEquals("pob", service.getLanguage("O pôr do sol na praia é sempre uma vista deslumbrante").block()?.languages?.get(0)?.lang);
+        assertEquals("fra", service.getLanguage("La vie est belle").block()?.languages?.get(0)?.lang);
+        assertEquals("ita", service.getLanguage("La dolce vita").block()?.languages?.get(0)?.lang);
+        assertEquals("eng", service.getLanguage("Artificial Intelligence is very cool").block()?.languages?.get(0)?.lang);
     }
 
 }
